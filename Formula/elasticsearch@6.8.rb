f0598f7ddc5d104e3bd193e82fc34865115de628
Adis Osmonov <adiosmon@amazon.co.uk>
2024-01-24 15:40:52 +0000

    init

---
+++ Formula/elasticsearch@6.8.rb
@@ -0,0 +1,121 @@
+class ElasticsearchAT68 < Formula
+  desc "Distributed search & analytics engine"
+  homepage "https://www.elastic.co/products/elasticsearch"
+  url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-6.8.23.tar.gz"
+  sha256 "60e77b5ca3ce11771469bcc2e009c49c8aadb831faebd170e7abcedc16b3e36d"
+  license "Apache-2.0"
+
+  keg_only :versioned_formula
+
+  # depends_on "openjdk" # old version is needed
+
+  def cluster_name
+    "elasticsearch_#{ENV["USER"]}"
+  end
+
+  def install
+    # Remove Windows files
+    rm_f Dir["bin/*.bat"]
+    rm_f Dir["bin/*.exe"]
+
+    # Install everything else into package directory
+    libexec.install "bin", "config", "lib", "modules"
+
+    inreplace libexec/"bin/elasticsearch-env",
+              "if [ -z \"$ES_PATH_CONF\" ]; then ES_PATH_CONF=\"$ES_HOME\"/config; fi",
+              "if [ -z \"$ES_PATH_CONF\" ]; then ES_PATH_CONF=\"#{etc}/elasticsearch\"; fi"
+
+    # Set up Elasticsearch for local development:
+    inreplace "#{libexec}/config/elasticsearch.yml" do |s|
+      # 1. Give the cluster a unique name
+      s.gsub!(/#\s*cluster\.name: .*/, "cluster.name: #{cluster_name}")
+
+      # 2. Configure paths
+      s.sub!(%r{#\s*path\.data: /path/to.+$}, "path.data: #{var}/lib/elasticsearch/")
+      s.sub!(%r{#\s*path\.logs: /path/to.+$}, "path.logs: #{var}/log/elasticsearch/")
+    end
+
+    inreplace "#{libexec}/config/jvm.options" do |s|
+      s.gsub! "logs/gc.log", "#{var}/log/elasticsearch/gc.log"
+      s.gsub! "10-:-XX:UseAVX=2", "# 10-:-XX:UseAVX=2" if Hardware::CPU.arm?
+    end
+
+    # Move config files into etc
+    (etc/"elasticsearch").install Dir[libexec/"config/*"]
+    (libexec/"config").rmtree
+
+    bin.install libexec/"bin/elasticsearch",
+                libexec/"bin/elasticsearch-keystore",
+                libexec/"bin/elasticsearch-plugin",
+                libexec/"bin/elasticsearch-translog"
+    bin.env_script_all_files(libexec/"bin", Language::Java.overridable_java_home_env)
+  end
+
+  def post_install
+    # Make sure runtime directories exist
+    (var/"lib/elasticsearch").mkpath
+    (var/"log/elasticsearch").mkpath
+    ln_s etc/"elasticsearch", libexec/"config" unless (libexec/"config").exist?
+    (var/"elasticsearch/plugins").mkpath
+    ln_s var/"elasticsearch/plugins", libexec/"plugins" unless (libexec/"plugins").exist?
+    # fix test not being able to create keystore because of sandbox permissions
+    system bin/"elasticsearch-keystore", "create" unless (etc/"elasticsearch/elasticsearch.keystore").exist?
+  end
+
+  service do
+    name macos: "#{plist_name}"
+    run opt_bin/"elasticsearch"
+    require_root false
+    working_dir var
+    log_path var/"log/elasticsearch.log"
+    error_log_path var/"log/elasticsearch.log"
+    # environment_variables ES_JAVA_HOME: "/Library/Java/JavaVirtualMachines/jdk-11.0.11.jdk/Contents/Home"
+  end
+
+  def caveats
+    <<~EOS
+      Data:    #{var}/lib/elasticsearch/
+      Logs:    #{var}/log/elasticsearch/#{cluster_name}.log
+      Plugins: #{var}/elasticsearch/plugins/
+      Config:  #{etc}/elasticsearch/
+    EOS
+  end
+
+  test do
+    assert_includes(stable.url, "-oss-")
+
+    port = free_port
+    system "#{bin}/elasticsearch-plugin", "list"
+    pid = testpath/"pid"
+    begin
+      system "#{bin}/elasticsearch", "-d", "-p", pid, "-Epath.data=#{testpath}/data", "-Ehttp.port=#{port}"
+      sleep 10
+      system "curl", "-XGET", "localhost:#{port}/"
+    ensure
+      Process.kill(9, pid.read.to_i)
+    end
+
+    port = free_port
+    (testpath/"config/elasticsearch.yml").write <<~EOS
+      path.data: #{testpath}/data
+      path.logs: #{testpath}/logs
+      node.name: test-es-path-conf
+      http.port: #{port}
+    EOS
+
+    cp etc/"elasticsearch/jvm.options", "config"
+    cp etc/"elasticsearch/log4j2.properties", "config"
+
+    ENV["ES_PATH_CONF"] = testpath/"config"
+    pid = testpath/"pid"
+    begin
+      system "#{bin}/elasticsearch", "-d", "-p", pid
+      sleep 10
+      system "curl", "-XGET", "localhost:#{port}/"
+      output = shell_output("curl -s -XGET localhost:#{port}/_cat/nodes")
+      assert_match "test-es-path-conf", output
+    ensure
+      Process.kill(9, pid.read.to_i)
+    end
+  end
+end
---
+++ README.md
@@ -0,0 +1,46 @@
+# Homebrew Tap
+
+If you would like to use formulas in this repository, start by tapping it.
+
+```bash
+brew tap adis-io/homebrew-tap
+```
+
+## Setup AMD64 Homebrew on your Apple Silicon Mac
+
+Some software that we use may only support AMD64 architecture. Therefore if
+you use a Mac with Apple Silicon you must also install one version of Homebrew
+for AMD64 if you need to use any software here that does not have support for
+ARM64.
+
+```bash
+softwareupdate --install-rosetta --agree-to-license
+arch -x86_64 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+```
+Then to use the AMD64 version of brew you should add an alias to ~/.zshrc. Use this command for that.
+
+*If you use bash, change ~/.zshrc to ~/.bashrc*
+
+```bash
+echo 'alias axbrew="arch -x86_64 /usr/local/homebrew/bin/brew"' >> ~/.zshrc
+```
+
+## Formulas
+
+### Elasticsearch@6.8
+
+Will install Elasticsearch version 6.8
+
+#### Using AMD64
+
+```bash
+brew install elasticsearch@6.8
+brew services start elasticsearch@6.8
+```
+
+#### Using ARM64 (Apple Silicon)
+
+```bash
+axbrew install elasticsearch@6.8
+axbrew services start elasticsearch@6.8
+```
